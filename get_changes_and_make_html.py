9ddad392314d656714ddd4f4ccac8cade0f1ee48
Brambor <tondadrd@seznam.cz>
2018-08-27 17:45:55 +0200

    Added support for comits without PR; changelog.html instead of pages/{n}.html

--- get_changes_and_make_html.py
+++ get_changes_and_make_html.py
@@ -9,18 +9,22 @@ from urllib.request import Request, urlopen
 
 
 pull_url = 'https://github.com/CleverRaven/Cataclysm-DDA/pull/{}'
+commit_url = 'https://github.com/CleverRaven/Cataclysm-DDA/commit/{}'
 driver = webdriver.Chrome(r'C:\Program Files\chromedriver.exe')  # if that throws error, fix this line
 
-if 'pages' not in os.listdir():
-	os.mkdir('pages')
 
-if os.listdir('pages'):  # if there is something
-	CDDA_version = int(os.listdir('pages')[-1][:-5]) + 1
+if 'changelog.html' in os.listdir():
+	with open('changelog.html', 'r') as changelog:
+		last_changes = changelog.readline()
+		CDDA_version = last_changes.split('</h2>')[0][4:]
+		CDDA_version = int(CDDA_version) + 1
 else:
 	req = Request('http://gorgon.narc.ro:8080/job/Cataclysm-Matrix/')
 	webpage_read = urlopen(req).read().decode("utf-8")
 	webpage_read = webpage_read.split('Last build (#')[1].split(')')[0]
 	CDDA_version = int(webpage_read)
+	with open('changelog.html', 'w') as changelog:
+		changelog.write('')
 
 first_try = True
 while True:
@@ -52,8 +56,9 @@ while True:
 
 	# follow commits, get pull requests
 	pulls = set()
+	no_pull_commits = set()
 	for commit in commit_set:
-		url = 'https://github.com/CleverRaven/Cataclysm-DDA/commit/{}'.format(commit)
+		url = commit_url.format(commit)
 
 		driver.get(url)
 		try:
@@ -61,6 +66,8 @@ while True:
 				EC.presence_of_element_located((By.CLASS_NAME, 'pull-request'))
 			).text[2:-1]
 		except:
+			commit_name = driver.find_element_by_class_name('commit-title').text
+			no_pull_commits.add((commit, commit_name))
 			continue
 		pulls.add(pull_number)
 
@@ -75,7 +82,7 @@ while True:
 	pulls_list.sort()
 
 	# make html file(s)
-	file_str = '''<h2>{v_current} <a href='file:///C:/Users/Tonda/Documents/GitHub/CDDA-changelog/pages/{v_next}.html'>&gt&gt</a></h2>'''.format(
+	file_str = '''<h2>{v_current}</h2>'''.format(
 		v_current=CDDA_version,
 		v_next=CDDA_version+1,
 		)
@@ -85,11 +92,22 @@ while True:
 			url=pull_url.format(pull_number),
 			name=pull_name,
 		)
-	if file_str[-4:] == '<br>':
-		file_str = file_str[:-4]
 
-	with open('pages/{}.html'.format(CDDA_version), 'w') as file:
-		file.write(file_str)
+	if no_pull_commits:
+		file_str += '<h3>Commits:</h3>'
+	for commit, commit_name in no_pull_commits:
+		file_str += '''<a href='{url}'>{name}<a><br>'''.format(
+			url=commit_url.format(commit),
+			name=commit_name,
+		)
+
+	with open('changelog.html', 'r') as changelog:
+		old_changelog = changelog.read()
+
+	file_str += '\n' + old_changelog
+
+	with open('changelog.html', 'w') as changelog:
+		changelog.write(file_str)
 
 	if further_version_exists:
 		CDDA_version += 1
